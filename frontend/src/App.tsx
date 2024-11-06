df7623b18d93dffa53b6a5e0d7cee681931be8e2
Mark Angelo <mark.jreyes107@gmail.com>
2024-11-06 21:00:38 +0800

    latest as of now 11/16/2024

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import HomeLayout from "./Layout/HomeLayout";
 
 function App() {
   return (
-    <div className="min-h-screen relative overflow-hidden flex items-center justify-center bg-[#D9CAC5]">
+    // <div className="min-h-screen relative overflow-hidden flex items-center justify-center bg-[#D9CAC5]">
+    <div className="">
       <Routes>
         <Route path="/" element={"Home"} />
 
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/main" element={<HomeLayout />} />
       </Routes>
     </div>
   );
