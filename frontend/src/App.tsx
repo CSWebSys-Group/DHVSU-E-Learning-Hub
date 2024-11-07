374efcb9ead10a41b9bee097bbd0a7b79ca15665
kieldoescode30 <kielycarreon123@gmail.com>
2024-11-07 17:48:32 +0800

    new structure, and responsiveness for login form (not final)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import LandingPage from "./pages/LandingPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="min-h-screen relative overflow-hidden flex items-center justify-center bg-[#D9CAC5]">
+    <div className="h-screen relative sm:flex sm:items-center sm:justify-center bg-[#D9CAC5]">
       <Routes>
-        <Route path="/" element={"Home"} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
