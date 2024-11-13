95af34c3f6f7afb61dfdb709d5b0a367ebbff3f7
yoitskyannu <kylenulud19@gmail.com>
2024-11-13 23:26:58 +0800

    Fixed Subject Seeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -23,5 +23,6 @@ class DatabaseSeeder extends Seeder
         $this->call(ValidIdSeeder::class);
         $this->call(CourseSeeder::class);
         $this->call(SectionSeeder::class);
+        $this->call(SubjectSeeder::class);
     }
 }
--- backend/database/seeders/TeacherSeeder.php
+++ backend/database/seeders/TeacherSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Teacher;
+
 
 class TeacherSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class TeacherSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Teacher::create([
+            'id' => 3992156069,
+            'fn' => 'John Mark',
+            'ln' => 'Policarpio',
+            'isAdmin' => 1,
+            'subjects' => json_encode([]),
+            'birthday' => '1990-01-01',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        
     }
 }
