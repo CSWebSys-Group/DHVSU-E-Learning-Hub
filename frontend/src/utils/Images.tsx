e1ba3ceee7cc447984364c46ad7e9fdfda002476
Sush1sui <jplovesraw@gmail.com>
2024-11-14 15:13:53 +0800

    destroyed frontend

--- frontend/src/utils/Images.tsx
+++
@@ -1,5 +0,0 @@
-import schoolLogo from "../assets/images/Don_Honorio_Ventura_State_University_logo.png";
-import schoolBg from "../assets/images/dhvsu-bg.png";
-
-export const dhvsuLogo = schoolLogo;
-export const dhvsuBg = schoolBg;
