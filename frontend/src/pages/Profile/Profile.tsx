0dac2202dc6ab110f5ef22a1ad339336c169728b
kieldoescode30 <kielycarreon123@gmail.com>
2024-11-21 07:09:47 +0800

    feat: added light and dark mode colors for profile and sidebar

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -1,17 +1,16 @@
 import { PencilIcon } from "lucide-react";
 import React from "react";
 
-// TODO: Not all input fields are are required.
-// TODO: Email disabled
-// TODO: Fix bg folors.
+// TODO: input should have borders (bg-dhvsu)
+// TODO: main & second container doesn't have light and dark mode
 
 const Profile = () => {
   return (
-    <div className="p-6">
+    <div className="p-6 flex items-center justify-center">
       <div className="flex gap-3">
-        <div className="w-96 ml-0 bg-form rounded-lg shadow-lg min-w-0">
+        <div className="w-96 ml-0 bg-form rounded-lg shadow-drop-1 min-w-0">
           <div className="bg-form rounded-t-lg p-6 flex flex-col items-center">
-            <div className="w-16 h-16 bg-DHVSU-white rounded-full flex items-center justify-center text-DHVSU-red font-semibold border border-DHVSU-red">
+            <div className="w-16 h-16 bg-DHVSU-white rounded-full flex items-center justify-center font-semibold border border-dhvsu dark:border-dhvsu-lighter">
               Image
             </div>
 
@@ -29,69 +28,50 @@ const Profile = () => {
           </div>
         </div>
 
-        <div className="flex-grow p-10 bg-DHVSU-light rounded-xl w-full shadow-md">
-          <span className="text-DHVSU-white font-semibold mx-3">
+        <div className="flex-grow p-10 bg-dhvsu-light dark:bg-dhvsu-lighter rounded-xl w-full shadow-md">
+          <span className="text-dhvsu-lighter font-semibold mx-3 dark:text-dhvsu-light">
             STUDENT INFORMATION
           </span>
-          <button className="bg-DHVSU-white text-DHVSU-light px-4 py-1 rounded-full font-semibold text-sm">
-            Personal
-          </button>
-          <button className="border border-DHVSU-white text-DHVSU-white px-4 py-1 rounded-full font-semibold text-sm">
-            Family Background
-          </button>
-          <button className="border border-DHVSU-white text-DHVSU-white px-4 py-1 rounded-full font-semibold text-sm">
-            Student ID
-          </button>
 
-          <div className="bg-DHVSU-light2 rounded-xl mx-0 my-2 w-full shadow-md p-6 space-y-4 min-w-0">
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+          <div className="bg-dhvsu-lighter dark:bg-dhvsu-light rounded-xl mx-0 my-2 w-full shadow-md p-6 space-y-4 min-w-0">
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-4 dark:text-white text-dhvsu-light">
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Last Name
-                </label>
+                <label className="font-semibold mb-2">Last Name</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full text-black"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  First Name
-                </label>
+                <label className="font-semibold mb-2">First Name</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full text-black"
                 />
               </div>
 
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Middle Name
-                </label>
+                <label className="font-semibold mb-2">Middle Name</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full text-black"
                 />
               </div>
 
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Ext.
-                </label>
+                <label className="font-semibold mb-2">Ext.</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full text-black"
                 />
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-4 text-dhvsu dark:text-white">
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Gender
-                </label>
-                <select className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full">
+                <label className=" font-semibold mb-2">Gender</label>
+                <select className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full text-black">
                   <option value="" disabled selected>
                     Select an option
                   </option>
@@ -101,31 +81,27 @@ const Profile = () => {
                 </select>
               </div>
 
-              <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
+              <div className="flex flex-col col-span-2 dark:text-white">
+                <label className=" font-semibold mb-2">
                   Date of Birth / Age
                 </label>
                 <input
                   type="date"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Place of Birth
-                </label>
+                <label className=" font-semibold mb-2">Place of Birth</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Civil Status
-                </label>
-                <select className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full">
+                <label className=" font-semibold mb-2">Civil Status</label>
+                <select className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full">
                   <option value="" disabled selected>
                     Select an option
                   </option>
@@ -136,128 +112,108 @@ const Profile = () => {
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-4 text-dhvsu-light dark:text-white">
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Nationality
-                </label>
+                <label className=" font-semibold mb-2">Nationality</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Religion
-                </label>
+                <label className=" font-semibold mb-2">Religion</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Email
-                </label>
+                <label className=" font-semibold mb-2">Email</label>
                 <input
                   type="email"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Contact #
-                </label>
+                <label className=" font-semibold mb-2">Contact #</label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-4 text-dhvsu-light dark:text-white">
               <div className="flex flex-col col-span-1">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Heigth (cm)
-                </label>
+                <label className=" font-semibold mb-2">Heigth (cm)</label>
                 <input
                   type="number"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Weight
-                </label>
+                <label className=" font-semibold mb-2">Weight</label>
                 <input
                   type="number"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
 
               <div className="flex flex-col col-span-2">
-                <label className="text-DHVSU-red font-semibold mb-2">
-                  Blood Type
-                </label>
+                <label className=" font-semibold mb-2">Blood Type</label>
                 <input
                   type="email"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+            <div className="grid grid-cols-1 md:grid-cols-6 gap-4 text-dhvsu-light dark:text-white">
               <div className="flex flex-col col-span-6">
-                <label className="text-DHVSU-red font-semibold mb-2">
+                <label className=" font-semibold mb-2">
                   Address (House #/Block/Street/Subdivision/Building)
                 </label>
                 <input
                   type="text"
-                  className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                  className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                 />
               </div>
             </div>
 
             <div>
-              <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-6 gap-4 text-dhvsu-light dark:text-white">
                 <div className="flex flex-col col-span-1">
-                  <label className="text-DHVSU-red font-semibold mb-2">
-                    Province
-                  </label>
+                  <label className=" font-semibold mb-2">Province</label>
                   <input
                     type="text"
-                    className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                    className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                   />
                 </div>
                 <div className="flex flex-col col-span-1">
-                  <label className="text-DHVSU-red font-semibold mb-2">
+                  <label className=" font-semibold mb-2">
                     Municipality / City
                   </label>
                   <input
                     type="text"
-                    className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                    className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                   />
                 </div>
                 <div className="flex flex-col col-span-1">
-                  <label className="text-DHVSU-red font-semibold mb-2">
-                    Barangay
-                  </label>
+                  <label className=" font-semibold mb-2">Barangay</label>
                   <input
                     type="text"
-                    className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                    className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                   />
                 </div>
                 <div className="flex flex-col col-span-1">
-                  <label className="text-DHVSU-red font-semibold mb-2">
-                    ZIP Code
-                  </label>
+                  <label className=" font-semibold mb-2">ZIP Code</label>
                   <input
                     type="number"
-                    className="border border-DHVSU-red rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
+                    className="border border-dhvsu rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-DHVSU-hover w-full"
                   />
                 </div>{" "}
               </div>
