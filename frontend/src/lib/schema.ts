287dee18c5c6b4b370971bf168f213594ba127ec
kieldoescode30 <kielycarreon123@gmail.com>
2024-11-27 21:42:25 +0800

    refactor: new inputs, new email and form validation, & new show password functionality

--- frontend/src/lib/schema.ts
+++ frontend/src/lib/schema.ts
@@ -1,34 +1,43 @@
 import { z } from "zod";
 
 export const registerSchema = z
-    .object({
-        fn: z.string().min(1, "First name is required"),
-        ln: z.string().min(1, "Last name is required"),
-        email: z.string().email(),
-        password: z
-            .string()
-            .min(8, { message: "Password must at least be 8 character(s)" })
-            .regex(/[A-Z]/, {
-                message: "Password must contain at least one uppercase letter",
-            })
-            .regex(/[!@#$%^&*(),.?":{}|<>]/, {
-                message: "Password must contain at least one special character",
-            }),
-        password_confirmation: z.string(),
-    })
-    .refine(
-        (data) => {
-            // if we return true, form is validated
-            // if we return false, form is not valid
-            return data.password === data.password_confirmation;
-        },
-        {
-            message: "Passwords do not match",
-            path: ["password_confirmation"],
-        }
-    );
+  .object({
+    id: z.string().min(10, "Please enter a valid Id"),
+    user_type: z.enum(["Teacher", "Student"], {
+      errorMap: () => ({ message: "Please select a option." }),
+    }),
+    fn: z.string().min(1, "First name is required"),
+    ln: z.string().min(1, "Last name is required"),
+    email: z
+      .string()
+      .email("Invalid email format")
+      .refine((email) => email.endsWith("@dhvsu.edu.ph"), {
+        message: "Email must end with @dhvsu.edu.ph",
+      }),
+    password: z
+      .string()
+      .min(8, { message: "Password must at least be 8 character(s)" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[!@#$%^&*(),.?":{}|<>]/, {
+        message: "Password must contain at least one special character",
+      }),
+    password_confirmation: z.string(),
+  })
+  .refine(
+    (data) => {
+      // if we return true, form is validated
+      // if we return false, form is not valid
+      return data.password === data.password_confirmation;
+    },
+    {
+      message: "Passwords do not match",
+      path: ["password_confirmation"],
+    }
+  );
 
 export const loginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(1, "Password is required"),
+  email: z.string().email(),
+  password: z.string().min(1, "Password is required"),
 });
